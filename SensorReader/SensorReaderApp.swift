60d40f903ea1c006dc19d11634dec7e85c13a3ce
Vid Tadel <tadelv@gmail.com>
2022-12-04 12:51:25 +0100

    Initial Commit

---
+++ SensorReader/SensorReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorReaderApp.swift
+//  SensorReader
+//
+//  Created by Vid Tadel on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SensorReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
